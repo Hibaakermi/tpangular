b578dab3e5dfe4e7f7d19816b4a4845bc910b726
Hibaakermi <hibatallahakermi@gmail.com>
2024-04-18 16:37:30 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitComponent } from './pages/produit/produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ProduitComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
